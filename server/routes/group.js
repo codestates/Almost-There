2207fcc41ed0fb61bcaa0703faa72d627a1f399c
torangL <yeonlee199900@gmail.com>
2022-02-15 16:32:42 +0900

    In progress: oauth

--- server/routes/group.js
+++ server/routes/group.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { groupController } = require('../controller');
 
 router.post('/create', groupController.create.post);
-router.delete('/delete', groupController.delete.delete);
+router.delete('/', groupController.delete.delete);
 router.post('/overtime', groupController.overtime.post);
 router.get('/list', groupController.list.get);
 
