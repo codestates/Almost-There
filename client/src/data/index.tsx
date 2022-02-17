b4f063c28d3a90260b54b6cb5bbb6eb27e3eb755
JHL71 <neko0717@naver.com>
2022-02-17 16:04:53 +0900

    edit img name

--- client/src/data/index.tsx
+++ client/src/data/index.tsx
@@ -1,6 +1,6 @@
-import logoN from './btnG_official.png';
-import logoG from './btn_google_signin_light_normal_web@2x.png';
-import logoK from './kakao_login_medium_narrow.png'
+import logoN from './naver_login.png';
+import logoG from './google_login.png';
+import logoK from './kakao_login.png'
 import projectLogo from './atlogo.png';
 
 export { projectLogo, logoN, logoG, logoK }
