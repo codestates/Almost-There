bb0f92097e8d169c84b8e5b4ea94d5b63f3bbaa4
torangL <yeonlee199900@gmail.com>
2022-02-18 11:35:52 +0900

    Modify endpoint

--- server/controller/user/index.js
+++ server/controller/user/index.js
@@ -2,7 +2,7 @@ module.exports = {
   login: require('./login'),
   logout: require('./logout'),
   signup: require('./signup'),
-  check: require('./check'),
+  check: require('./check-id'),
   delete: require('./delete'),
   updateInfo: require('./updateInfo'),
   updatePassword: require('./updatePassword'),
