8d40be1ea421420882bc6c7d54aaa40232d01f71
torangL <yeonlee199900@gmail.com>
2022-02-22 09:30:54 +0900

    in progress: socket.io

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
-const { Server } = require('socket.io');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
 const app = express();
+const httpServer = createServer(app);
 const PORT = process.env.PORT || 4000;
 const userRouter = require('./routes/user');
 const groupRouter = require('./routes/group');
@@ -33,13 +35,31 @@ app.use('/group', groupRouter);
 app.use('/notification', notificationRouter);
 app.use('/social', socialRouter);
 
-const io = new Server(app.listen(PORT, () => {
-  console.log(`HTTP server listen on ${PORT}`);
-}), {}
-);
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+  }
+});
 
 io.on('connection', (socket) => {
+  console.log('connected');
+  socket.on('send message', (item) => {
+    const msg = item.name + ': ' + item.message;
+    console.log(msg);
+    io.emit('receive message', { name: item.name, message: item.message });
+  });
+  socket.on('disconnect', () => {
+    console.log('disconnected', socket.id);
+  });
   // ...
+});
+
+io.emit('success', '서버에서 메시지 보냄');
+
+app.listen(PORT, () => {
+  console.log(`HTTP server listen on ${PORT}`);
 });
 
 // const fs = require("fs");
