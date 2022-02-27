7438d75b4da99d992a6f7686c02b787a995bc4fc
JHL71 <neko0717@naver.com>
2022-02-27 15:34:57 +0900

    fix lint

--- client/src/component/timer.tsx
+++ client/src/component/timer.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import '../App.tsx';
 import styled from "styled-components";
 import {useParams } from 'react-router-dom';
@@ -10,50 +10,26 @@ import axios from "axios";
 function Timer () {
 
   const [boo, setBoo] = useState<boolean>(false);
-  const title = useRef<HTMLParagraphElement>(null); 
   const[day,setDay] = useState<any>(0)
   const[seconds,setSeconds] = useState<any>(0)
   const [minutes,setMinutes] =useState<any> (0);
-  const [isIncrease,setIsIncrease] = useState(false);
-  
   const [hours,setHours] = useState<any>(0);
   // Math.floor((1 % (1000*60*60*24))/(1000*60*60));
   const [groupTime, setGroupTime] = useState<Array<any>>([]);
   const params= useParams();
-  console.log(day, seconds, minutes, hours)
-  // setDate = new Date(time);
-  // 131669123
-
-
-
-
 
   const getGroupTime = async () => {
     const res = await axios.get(`${url}/group/list`,{withCredentials:true});
     const filtered = res.data.groups.filter((el:any)=>{
       return el.id === Number(params.id);    
-    });    
-    // setGroupTime([...filtered]);
-    // console.log(filtered)
-    // setTime(groupTime[0]?._group.time)
-    // console.log((time))
-      let setDate = new Date(filtered[0]?._group.time);
-      const now = new Date();
-      console.log((setDate))
-      console.log(groupTime[0]?._group.time)
-          console.log((now))
-      // setDistance(setDate.getTime() - now.getTime())
-    // console.log(groupTime[0]._group.time)
-    // 2022-02-28T18:00:00.000Z
+    });
+    let setDate = new Date(filtered[0]?._group.time);
+    const now = new Date();
     setBoo(true)
     setDay(Math.floor((setDate.getTime() - now.getTime())/(1000*60*60*24)))
     setMinutes(Math.floor(((setDate.getTime() - now.getTime()) % (1000*60*60))/(1000*60)))
     setSeconds(Math.floor((setDate.getTime() - now.getTime())%(1000*60)/(1000)))
   }
-
-
-  // getGroupTime()
-
 
   function minusOne (){
     if(seconds ===0){
@@ -66,27 +42,6 @@ function Timer () {
     // console.log('test')
   }
 
-
-  const getDDay = () => {
-
-  //   const setDateYear = setDate.getFullYear();
-  // const setDateMonth = setDate.getMonth() + 1;
-  // const setDateDay = setDate.getDate();
-  // if(title.current){
-  //   title.current.innerText = 
-  //   `${setDateYear}년 ${setDateMonth}월 ${setDateDay}일까지 
-  //     ${day}일 
-  //     ${hours < 10 ? `0${hours}` : hours}시간 
-  //     ${minutes < 10 ? `0${minutes}` : minutes}분 
-  //     ${seconds < 10 ? `0${seconds}` : seconds}초 남았습니다.`;
-  //   }
-  }
-// const init = () => {
-
-//   // init 함수 생성해서 getDDay함수 호출하고,
-//   getDDay();
-//   setTimeout(minusOne, 1000);
-//   }
   useEffect(() => {
     if(boo){
       setTimeout(minusOne, 1000);
@@ -100,24 +55,24 @@ function Timer () {
 
   return(
     <>
-  <Contents2>
-    <TimeBox>
-      <IntBox>{day}</IntBox>
-      <TextBox>일</TextBox>
-    </TimeBox>
-    <TimeBox>
-      <IntBox>{hours}</IntBox>
-      <TextBox>시간</TextBox>
-    </TimeBox>
-    <TimeBox>
-      <IntBox>{minutes}</IntBox>
-      <TextBox>분</TextBox>
-    </TimeBox>
-    <TimeBox>
-      <IntBox>{seconds}</IntBox>
-      <TextBox>초</TextBox>
-    </TimeBox>
-  </Contents2>
+      <Contents2>
+        <TimeBox>
+          <IntBox>{day}</IntBox>
+          <TextBox>일</TextBox>
+        </TimeBox>
+        <TimeBox>
+          <IntBox>{hours}</IntBox>
+          <TextBox>시간</TextBox>
+        </TimeBox>
+        <TimeBox>
+          <IntBox>{minutes}</IntBox>
+          <TextBox>분</TextBox>
+        </TimeBox>
+        <TimeBox>
+          <IntBox>{seconds}</IntBox>
+          <TextBox>초</TextBox>
+        </TimeBox>
+      </Contents2>
     </>
   )
   
